89a70e30b21520bada8501b7aecf63fd39f76807
AnaCristina Cantu <anacantu1@gmail.com>
2015-11-05 21:49:16 -0600

    primer ejercicio de javascript piedra, papel, tijera

---
+++ juego.js
@@ -0,0 +1,19 @@
+var usuario = prompt ("¿que eliges: piedra, papel o tijera?")
+
+var computadora = "piedra"
+
+if (computadora == usuario) {
+	console.log("empate")
+};
+if (usuario == "papel") {
+	console.log("ganaste")
+};
+if (usuario
+ == "tijera") {
+	console.log("perdiste")
+};
+
+
+
+
+
